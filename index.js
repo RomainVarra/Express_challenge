2cc828a3954a98c7a6390b1a8fbcb68732b426da
Romain Varra <romain.varra@hotmail.com>
2024-10-30 21:03:00 +0100

    feat: end of exercice

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: ["http://localhost:3000","http://localhost:5174"] }));
 const sampleEmployee = {
     name: {
       first: "Charlie",
